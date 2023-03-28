dded3399ce47e1762a81a6ad266bb4c67c9555df
Marouan El Yattafti <marouanely@gmail.com>
2023-03-28 15:27:38 +0200

    bug fix

--- components/ProfileSection.js
+++ components/ProfileSection.js
@@ -6,9 +6,12 @@ const ProfileSection = ({ items, title }) => {
       <div className='text-lg font-semibold'>{title}</div>
       <div className='grid grid-cols-6'>
         {/* Track */}
-        {items?.map((item) => {
+        {items?.map((item, i) => {
           return (
-            <div className=' col-span-1 items-center rounded-lg bg-background-light p-3 pb-0'>
+            <div
+              key={i}
+              className=' col-span-1 items-center rounded-lg bg-background-light p-3 pb-0'
+            >
               <img src={item.image} className='w-full rounded-md' />
               <div className='py-2 text-center'>{item.name}</div>
             </div>

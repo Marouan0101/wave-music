4456f0f8550755c9127ffd65dabf44368f214d7f
Marouan El Yattafti <marouanely@gmail.com>
2023-03-29 14:55:06 +0200

    bug fix

--- components/ProfileSection.js
+++ components/ProfileSection.js
@@ -10,7 +10,7 @@ const ProfileSection = ({ items, title }) => {
           return (
             <div
               key={i}
-              className=' col-span-1 items-center rounded-lg bg-background-light p-3 pb-0'
+              className='col-span-1 items-center rounded-lg bg-background-light p-3 pb-0'
             >
               <img src={item.image} className='w-full rounded-md' />
               <div className='py-2 text-center'>{item.name}</div>

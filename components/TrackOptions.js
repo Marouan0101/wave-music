88c0fa29636f065312cec12d159c48b960c7ed42
Marouan El Yattafti <marouanely@gmail.com>
2023-04-11 12:31:39 +0200

    updated track options

--- components/TrackOptions.js
+++ components/TrackOptions.js
@@ -10,16 +10,17 @@ const TrackOptions = ({ track }) => {
       <>
         <RxDotsHorizontal
           onClick={() => setIsOpen(!isOpen)}
-          className='h-7 w-7 cursor-pointer text-primary transition-all hover:scale-105 '
+          className='h-7 w-7 cursor-pointer text-primary shadow-xl transition-all hover:scale-105 '
         />
-        <div className='absolute bottom-20 right-0 w-72 rounded-xl bg-black'>
+        <div className='absolute bottom-20 right-0 w-40 space-y-2 rounded-xl bg-black p-4 text-right'>
           <div
             onClick={() => addTrackToQueue(track)}
-            className='cursor-pointer border-b border-grey-dark'
+            className='cursor-pointer text-sm'
           >
             Add to queue
           </div>
-          <div className='cursor-default'>Add to playlist</div>
+          <hr className='w-full border-grey-dark' />
+          <div className='cursor-default text-sm'>Add to playlist</div>
         </div>
       </>
     );

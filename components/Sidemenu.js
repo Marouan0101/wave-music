71abb8696cadaa0b1f429c9c61650aa4232fde6b
Marouan El Yattafti <marouanely@gmail.com>
2023-04-13 12:00:40 +0200

    added custom scrollbar to sidemenu

--- components/Sidemenu.js
+++ components/Sidemenu.js
@@ -39,34 +39,34 @@ const Sidemenu = ({ name, image }) => {
   //console.log('User', user);
   //console.log('Playlists', playlists);
   return (
-    <div className="absolute top-0 left-0 h-full w-[16%] border-r border-grey-dark px-3">
+    <div className='absolute top-0 left-0 h-full w-[16%] overflow-y-auto border-r border-grey-dark px-3 pb-24'>
       {/* Profile */}
       <div>
-        <Link id="sidemenu-profile" href="/profile">
-          <div className="flex cursor-pointer  items-center space-x-3 py-3 transition-all duration-300 ease-out hover:text-primary">
-            <div className="relative h-12 w-12">
-              <div className="absolute h-[3.125rem] w-[3.125rem] -translate-x-[0.0625rem] -translate-y-[0.0625rem] rounded-full bg-gradient-to-br from-primary to-secondary"></div>
-              <img className="absolute rounded-full" src={image} />
+        <Link id='sidemenu-profile' href='/profile'>
+          <div className='flex cursor-pointer  items-center space-x-3 py-3 transition-all duration-300 ease-out hover:text-primary'>
+            <div className='relative h-12 w-12'>
+              <div className='absolute h-[3.125rem] w-[3.125rem] -translate-x-[0.0625rem] -translate-y-[0.0625rem] rounded-full bg-gradient-to-br from-primary to-secondary'></div>
+              <img className='absolute rounded-full' src={image} />
 
               <AiFillSetting
                 onClick={() => setIsProfileOpen(!isProfileOpen)}
-                className="icon-settings absolute right-1/2 bottom-1/2 z-50 h-5 w-5 translate-x-1/2 translate-y-1/2 text-grey-light opacity-0 transition-all duration-200"
+                className='icon-settings absolute right-1/2 bottom-1/2 z-50 h-5 w-5 translate-x-1/2 translate-y-1/2 text-grey-light opacity-0 transition-all duration-200'
               />
             </div>
 
-            <div className="text-xl font-semibold">{name}</div>
+            <div className='text-xl font-semibold'>{name}</div>
           </div>
         </Link>
 
         {isProfileOpen && (
-          <div className="absolute z-50 space-y-1 rounded-lg bg-background-light p-2 text-sm shadow-lg shadow-black">
-            <div className="cursor-pointer hover:underline">Account</div>
+          <div className='absolute z-50 space-y-1 rounded-lg bg-background-light p-2 text-sm shadow-lg shadow-black'>
+            <div className='cursor-pointer hover:underline'>Account</div>
 
-            <div className=" cursor-pointer hover:underline">Preferences</div>
+            <div className=' cursor-pointer hover:underline'>Preferences</div>
 
             <div
               onClick={async () => await signOut(auth)}
-              className="cursor-pointer hover:underline"
+              className='cursor-pointer hover:underline'
             >
               Log out
             </div>
@@ -75,18 +75,18 @@ const Sidemenu = ({ name, image }) => {
       </div>
 
       {/* Sidebar links */}
-      <div className="space-y-6 py-4 font-semibold ">
-        <SidemenuLink text="Home" icon={HomeIcon.src} url="/" />
-        <SidemenuLink text="Liked tracks" icon={HeartIcon.src} url="/1" />
-        <SidemenuLink text="Playlists" icon={PlaylistIcon.src} url="/2" />
+      <div className='space-y-6 py-4 font-semibold '>
+        <SidemenuLink text='Home' icon={HomeIcon.src} url='/' />
+        <SidemenuLink text='Liked tracks' icon={HeartIcon.src} url='/1' />
+        <SidemenuLink text='Playlists' icon={PlaylistIcon.src} url='/2' />
 
         <CreatePlaylist user={user} />
       </div>
 
-      <hr className="my-3 mb-6 border-grey-dark" />
+      <hr className='my-3 mb-6 border-grey-dark' />
 
       {/* Playlists */}
-      <div className="overflow-y-aut space-y-2">
+      <div className='overflow-y-aut space-y-2'>
         {playlists?.map((playlist) => {
           return (
             <SidemenuPlaylist

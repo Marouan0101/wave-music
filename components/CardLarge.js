2ecf0a9078abea10b6ac4017636f4052bdc8bcc7
Jeremy Brandt <jeremybrandt@jbstudio.dk>
2023-04-13 13:36:56 +0200

    Started on create song preview

--- components/CardLarge.js
+++ components/CardLarge.js
@@ -4,24 +4,24 @@ import { playTrack } from '../firebase/getPlayer';
 
 const CardLarge = ({ track }) => {
   return (
-    <div className='component items-center rounded-lg bg-background-light p-3  pb-0 shadow-md transition-all hover:scale-105 hover:shadow-2xl'>
-      <div className='relative'>
-        <img src={track.image} className='rounded-md object-cover' />
+      <div className="component items-center rounded-lg bg-background-light p-3  pb-0 shadow-md transition-all hover:scale-105 hover:shadow-2xl">
+          <div className="relative">
+              <img src={track?.image} className="rounded-md object-cover" />
 
-        <div
-          onClick={() => playTrack(track)}
-          className='component-play absolute bottom-1/2 left-1/2 -translate-x-1/2 translate-y-1/2 cursor-pointer rounded-full bg-gradient-to-br from-primary to-secondary p-2 opacity-0 shadow-lg transition-all hover:scale-105 hover:shadow-xl'
-        >
-          <PlayIcon className='h-8 w-8 text-white' />
-        </div>
+              <div
+                  onClick={() => playTrack(track)}
+                  className="component-play absolute bottom-1/2 left-1/2 -translate-x-1/2 translate-y-1/2 cursor-pointer rounded-full bg-gradient-to-br from-primary to-secondary p-2 opacity-0 shadow-lg transition-all hover:scale-105 hover:shadow-xl"
+              >
+                  <PlayIcon className="h-8 w-8 text-white" />
+              </div>
+          </div>
+          <div className="py-2 text-center">
+              <div className="text-base font-semibold">{track?.name}</div>
+              <div className="text-sm font-light text-grey-light">
+                  {track?.artists[0].name}
+              </div>
+          </div>
       </div>
-      <div className='py-2 text-center'>
-        <div className='text-base font-semibold'>{track.name}</div>
-        <div className='text-sm font-light text-grey-light'>
-          {track.artists[0].name}
-        </div>
-      </div>
-    </div>
   );
 };
 
--- components/CreateTrackModal.js
+++ components/CreateTrackModal.js
@@ -1,14 +1,30 @@
 import React from 'react'
 import { TbFileUpload } from "react-icons/tb";
 import { BsFileEarmarkImage } from "react-icons/bs";
+import CardLarge from "../components/CardLarge";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebase/auth";
 
 const CreateTrackModal = () => {
+    const [user, loading] = useAuthState(auth);
+
+    const previewTrack = {
+        image: null,
+        name: null,
+        artists: [
+            {
+                name: user?.displayName,
+            },
+        ],
+    };
     return (
         /* Body */
         <div className="absolute z-50 h-full w-[84%] bg-black/80">
             <div className="m-auto mt-10  h-5/6 w-1/2 rounded-3xl bg-background-light p-8 shadow-lg">
                 {/* Preview section */}
-                <div>Create song shit</div>
+                <div>
+                    <CardLarge track={previewTrack} />
+                </div>
                 {/* Input section */}
                 <div className="grid grid-cols-2 items-center gap-8">
                     {/* Song name inputfield */}

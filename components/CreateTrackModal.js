02f6b73d06931256802dc5c691a70628d149aae2
Jeremy Brandt <jeremybrandt@jbstudio.dk>
2023-04-14 20:33:02 +0200

    type error

--- components/CreateTrackModal.js
+++ components/CreateTrackModal.js
@@ -102,7 +102,7 @@ const CreateTrackModal = () => {
                                 className="w-full rounded-full bg-slate-100 py-2 px-4 text-black outline-none"
                             />
                         </div>
-                        {/* Inout collabs */}
+                        {/* Input collabs */}
                         <div className="col-span-2 space-y-2">
                             <div className="font-semibold">Collaboraters</div>
                             <input

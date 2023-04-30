2099c6e650795ae6739651daf6ef7c90a42a9b9c
Marouan El Yattafti <marouanely@gmail.com>
2023-04-30 20:07:37 +0200

    added comments

--- components/Player.js
+++ components/Player.js
@@ -64,7 +64,7 @@ const Player = () => {
         setProgressWidth(percentage * 100);
       }
     }, 0);
-    return () => clearInterval(intervalId);
+    return () => clearInterval(intervalId); // clear the interval when the component unmounts
   }, [audio]);
 
   const handlePlay = async () => {
@@ -94,97 +94,86 @@ const Player = () => {
   }
 
   return (
-      <div className="fixed bottom-4 left-1/2 z-50 w-11/12 -translate-x-1/2 rounded-xl bg-purple-dark shadow-xl transition-all duration-300">
-          {/* ProgressLine */}
-          <div
-              id="progressLine"
-              className="h-1 rounded-full  bg-gradient-to-r from-transparent via-primary/50 to-primary"
-              style={{ width: `${progressWidth}%` }} // width is set to the percentage of the current time of the track
-          ></div>
+    <div className='fixed bottom-4 left-1/2 z-50 w-11/12 -translate-x-1/2 rounded-xl bg-purple-dark shadow-xl transition-all duration-300'>
+      {/* ProgressLine */}
+      <div
+        id='progressLine'
+        className='h-1 rounded-full  bg-gradient-to-r from-transparent via-primary/50 to-primary'
+        style={{ width: `${progressWidth}%` }} // width is set to the percentage of the current time of the track
+      ></div>
 
-          <div className="grid grid-cols-3">
-              {/* left */}
-              <div className="col-span-1 flex items-center">
-                  <img
-                      src={queue?.currentTrack.image}
-                      className="h-16 w-16 rounded-bl-xl object-cover"
-                  />
+      <div className='grid grid-cols-3'>
+        {/* left */}
+        <div className='col-span-1 flex items-center'>
+          <img
+            src={queue?.currentTrack.image}
+            className='h-16 w-16 rounded-bl-xl object-cover'
+          />
 
-                  <div className="space-y-1 px-2">
-                      <div className="font-semibold">
-                          {queue?.currentTrack.name}
-                      </div>
+          <div className='space-y-1 px-2'>
+            <div className='font-semibold'>{queue?.currentTrack.name}</div>
 
-                      <div className="flex space-x-1">
-                          {/* loop through the current track's artists */}
-                          {queue?.currentTrack.artists.map((artist, i) => {
-                              // if it's the last artist:
-                              if (i + 1 === queue.currentTrack.artists.length) {
-                                  // return the artist's name, without a comma
-                                  return (
-                                      <div
-                                          key={artist.uid}
-                                          className="text-sm text-grey-light"
-                                      >
-                                          {artist.name}
-                                      </div>
-                                  );
-                              }
-                              // if it's not the last artist:
-                              else {
-                                  // return the artist's name, with a comma
-                                  return (
-                                      <div
-                                          className="text-sm text-grey-light"
-                                          key={artist.uid}
-                                      >
-                                          {artist.name},
-                                      </div>
-                                  );
-                              }
-                          })}
-                      </div>
-                  </div>
-
-                  <img src={HeartIcon.src} className="ml-10 h-5 w-5" />
-              </div>
-
-              {/* Middle */}
-              <div className="col-span-1 flex items-center justify-between">
-                  <IoShuffleOutline className="h-7 w-7 cursor-pointer text-grey-light transition-all hover:scale-105 hover:text-white" />
-                  <IoPlaySkipBack className="h-7 w-7 cursor-pointer text-grey-light transition-all hover:scale-105 hover:text-white" />
-
-                  {/* if the player is playing: display the pause icon, else display the play icon */}
-                  {states.isPlaying ? (
-                      <div
-                          className="cursor-pointer rounded-full bg-gradient-to-br from-primary to-secondary p-1.5 transition-all hover:scale-105 active:scale-100"
-                          onClick={handlePause}
-                      >
-                          <PauseIcon className="h-7 w-7 text-purple-dark" />
-                      </div>
-                  ) : (
-                      <div
-                          className="cursor-pointer rounded-full bg-gradient-to-br from-primary to-secondary p-1.5 transition-all hover:scale-105 active:scale-100"
-                          onClick={handlePlay}
-                      >
-                          <PlayIcon className="h-7 w-7 text-purple-dark" />
-                      </div>
-                  )}
-
-                  <IoPlaySkipForward className="h-7 w-7 cursor-pointer text-grey-light transition-all hover:scale-105 hover:text-white" />
-                  <IoRepeatOutline className="h-7 w-7 cursor-pointer text-grey-light transition-all hover:scale-105 hover:text-white" />
-              </div>
-
-              {/* Right */}
-              <div className="col-span-1 flex items-center justify-end space-x-4 pr-4">
-                  <Queue
-                      tracks={queue.tracks}
-                      currentTrack={queue.currentTrack}
-                  />
-                  <TrackOptions track={queue.currentTrack} />
-              </div>
+            <div className='flex space-x-1'>
+              {/* loop through the current track's artists */}
+              {queue?.currentTrack.artists.map((artist, i) => {
+                // if it's the last artist:
+                if (i + 1 === queue.currentTrack.artists.length) {
+                  // return the artist's name, without a comma
+                  return (
+                    <div key={artist.uid} className='text-sm text-grey-light'>
+                      {artist.name}
+                    </div>
+                  );
+                }
+                // if it's not the last artist:
+                else {
+                  // return the artist's name, with a comma
+                  return (
+                    <div className='text-sm text-grey-light' key={artist.uid}>
+                      {artist.name},
+                    </div>
+                  );
+                }
+              })}
+            </div>
           </div>
+
+          <img src={HeartIcon.src} className='ml-10 h-5 w-5' />
+        </div>
+
+        {/* Middle */}
+        <div className='col-span-1 flex items-center justify-between'>
+          <IoShuffleOutline className='h-7 w-7 cursor-pointer text-grey-light transition-all hover:scale-105 hover:text-white' />
+          <IoPlaySkipBack className='h-7 w-7 cursor-pointer text-grey-light transition-all hover:scale-105 hover:text-white' />
+
+          {/* if the player is playing: display the pause icon, else display the play icon */}
+          {states.isPlaying ? (
+            <div
+              className='cursor-pointer rounded-full bg-gradient-to-br from-primary to-secondary p-1.5 transition-all hover:scale-105 active:scale-100'
+              onClick={handlePause}
+            >
+              <PauseIcon className='h-7 w-7 text-purple-dark' />
+            </div>
+          ) : (
+            <div
+              className='cursor-pointer rounded-full bg-gradient-to-br from-primary to-secondary p-1.5 transition-all hover:scale-105 active:scale-100'
+              onClick={handlePlay}
+            >
+              <PlayIcon className='h-7 w-7 text-purple-dark' />
+            </div>
+          )}
+
+          <IoPlaySkipForward className='h-7 w-7 cursor-pointer text-grey-light transition-all hover:scale-105 hover:text-white' />
+          <IoRepeatOutline className='h-7 w-7 cursor-pointer text-grey-light transition-all hover:scale-105 hover:text-white' />
+        </div>
+
+        {/* Right */}
+        <div className='col-span-1 flex items-center justify-end space-x-4 pr-4'>
+          <Queue tracks={queue.tracks} currentTrack={queue.currentTrack} />
+          <TrackOptions track={queue.currentTrack} />
+        </div>
       </div>
+    </div>
   );
 };
 

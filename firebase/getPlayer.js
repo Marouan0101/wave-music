110d2de7ca7e6151293f0fcd443a414f9fc442f1
Jeremy Brandt <jeremybrandt@jbstudio.dk>
2023-05-02 14:32:29 +0200

    add to queue toast

--- firebase/getPlayer.js
+++ firebase/getPlayer.js
@@ -1,42 +1,44 @@
-import { db, playerRef } from './firestore';
-import { getDocs, doc, updateDoc } from 'firebase/firestore';
+import { toast } from "react-toastify";
+import { db, playerRef } from "./firestore";
+import { getDocs, doc, updateDoc } from "firebase/firestore";
 
 // get queue data
 const getQueue = async () => {
-  const playerQueue = await getDocs(playerRef)
-    .then((snapshot) => {
-      return snapshot.docs[0].data();
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
-  return playerQueue;
+    const playerQueue = await getDocs(playerRef)
+        .then((snapshot) => {
+            return snapshot.docs[0].data();
+        })
+        .catch((err) => {
+            console.log(err.message);
+        });
+    return playerQueue;
 };
 
 // get player state
 const getState = async () => {
-  const playerState = await getDocs(playerRef)
-    .then((snapshot) => {
-      return snapshot.docs[1].data();
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
-  return playerState;
+    const playerState = await getDocs(playerRef)
+        .then((snapshot) => {
+            return snapshot.docs[1].data();
+        })
+        .catch((err) => {
+            console.log(err.message);
+        });
+    return playerState;
 };
 
 const playTrack = async (track) => {
-  await updateDoc(doc(db, 'player/queue'), {
-    currentTrack: track,
-  });
+    await updateDoc(doc(db, "player/queue"), {
+        currentTrack: track,
+    });
 };
 
 const addTrackToQueue = async (track) => {
-  await getQueue().then(async (queue) => {
-    await updateDoc(doc(db, 'player/queue'), {
-      tracks: [...queue.tracks, track],
+    await getQueue().then(async (queue) => {
+        await updateDoc(doc(db, "player/queue"), {
+            tracks: [...queue.tracks, track],
+        });
     });
-  });
+    toast.success("Track added to queue");
 };
 
 export { getState, getQueue, playTrack, addTrackToQueue };
